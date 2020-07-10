6df6a70f57e19c476283f77760af6135acfea102
IgorokNerovnyi1980 <i.nerovnyi@gmail.com>
2020-07-10 12:34:18 +0300

    added .env and start script

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser());
-const port = "https://hotdogs-server.herokuapp.com";
+const port = process.env.DOMAIN || 3000;
 const way = "./src/newDATA.json";
 
 let datas = null;
